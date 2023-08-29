986176ec4ab438d79ea04c77fbcb6aad27df04eb
rrodrigodlima <rdrigo019@gmail.com>
2023-08-29 12:55:02 -0300

    requirement 5

--- src/services/inventory_control.py
+++ src/services/inventory_control.py
@@ -27,8 +27,17 @@ class InventoryMapping:
 
     # Req 5.1
     def check_recipe_availability(self, recipe: Recipe) -> bool:
-        pass
+        for ingredient, amount in recipe.items():
+            if ingredient not in self.inventory or \
+                    self.inventory[ingredient] < amount:
+                return False
+        return True
 
     # Req 5.2
     def consume_recipe(self, recipe: Recipe) -> None:
-        pass
+        for ingredient, amount in recipe.items():
+            if ingredient in self.inventory \
+                    and self.inventory[ingredient] >= amount:
+                self.inventory[ingredient] -= amount
+            else:
+                raise ValueError('insufficient amount')
