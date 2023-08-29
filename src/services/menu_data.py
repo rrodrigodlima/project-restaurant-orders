91cb93496f3f9be85f27ed2719397c06ab75a3eb
rrodrigodlima <rdrigo019@gmail.com>
2023-08-29 12:48:15 -0300

    requirement 3

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,26 @@
-# Req 3
+from src.models.dish import Dish
+from src.models.ingredient import Ingredient
+import csv
+
+
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        pass
+        self.source_path = source_path
+        self.dishes = self.read_csv()
+
+    def read_csv(self):
+        dish_set = set()
+        dish_dict = {}
+        with open(self.source_path, 'r') as csv_file:
+            content = csv.DictReader(csv_file)
+            for row in content:
+                if row['dish'] not in dish_set:
+                    dish_set.add(row['dish'])
+                    dish_dict[row['dish']] = Dish(
+                        row['dish'], float(row['price'])
+                    )
+                ingredient = Ingredient(row['ingredient'])
+                dish_dict[row['dish']].add_ingredient_dependency(
+                    ingredient, int(row['recipe_amount'])
+                )
+        return list(dish_dict.values())
